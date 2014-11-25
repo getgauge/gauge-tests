dd8ea934c662b0facc881bfab1c81b760e5ca8d8
ApoorvaM <apoorva_rarefriend@yahoo.com>
2014-11-25 15:05:26 +0530

    Changing ruby impl for exception

--- src/test/java/common/RubyProject.java
+++ src/test/java/common/RubyProject.java
@@ -58,7 +58,7 @@ public class RubyProject extends GaugeProject {
             builder.append("\n");
         }
         else if(implementation.toLowerCase().equals(THROW_EXCEPTION)){
-            builder.append("raise \" exception raised \" \n");
+            builder.append("def \n raise \" exception raised \" \nend \n");
         }
         else {
             builder.append("puts ").append(implementation).append("\n");
