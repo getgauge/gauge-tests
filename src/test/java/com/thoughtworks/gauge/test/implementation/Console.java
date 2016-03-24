e0cc256a6562df563756226a2b65afd71da6c3a3
sriv <srikanth.ddit@gmail.com>
2016-03-24 11:27:36 +0530

    refactored execution assertions

--- src/test/java/com/thoughtworks/gauge/test/implementation/Console.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/Console.java
@@ -44,5 +44,4 @@ public class Console {
             assertThat(output).contains(s);
         }
     }
-
 }
--- src/test/java/com/thoughtworks/gauge/test/implementation/Execution.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/Execution.java
@@ -6,126 +6,113 @@ import com.thoughtworks.gauge.test.common.Specification;
 import java.io.IOException;
 
 import static com.thoughtworks.gauge.test.common.GaugeProject.currentProject;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Execution {
 
     @Step("Execute the current project and ensure success")
     public void executeCurrentProjectAndEnsureSuccess() throws Exception {
-        assertTrue(isExecutionPassed());
+        assertThat(currentProject.execute(false))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the current project in parallel and ensure success")
     public void executeCurrentProjectInParallelAndEnsureSuccess() throws IOException, InterruptedException {
-        boolean passed = currentProject.executeInParallel();
-        printProcessOutput(passed);
-        assertTrue(passed);
+        assertThat(currentProject.executeInParallel())
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the current project in parallel in <n> streams and ensure success")
     public void executeCurrentProjectInParallelStreamsAndEnsureSuccess(int n) throws IOException, InterruptedException {
-        boolean passed = currentProject.executeInParallel(n);
-        printProcessOutput(passed);
-        assertTrue(passed);
+        assertThat(currentProject.executeInParallel(n))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the specs in order and ensure success")
     public void executeSpecsInOrderAndEnsureSuccess() throws Exception {
-        assertTrue(isExecutionPassed(true));
+        assertThat(currentProject.execute(true))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the current project and ensure failure")
     public void executeCurrentProjectAndEnsureFailure() throws Exception {
-        assertTrue(!isExecutionPassed());
-    }
-
-    private boolean isExecutionPassed() throws Exception {
-        boolean passed = currentProject.execute(false);
-        printProcessOutput(passed);
-        return passed;
-    }
-
-    private boolean isExecutionPassed(boolean sorted) throws Exception {
-        boolean passed = currentProject.execute(sorted);
-        printProcessOutput(passed);
-        return passed;
+        assertThat(currentProject.execute(false))
+                .isFalse()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the spec <spec name> and ensure success")
     public void executeSpecAndEnsureSuccess(String specName) throws Exception {
-        boolean passed = executeSpec(specName);
-        printProcessOutput(passed);
-        assertTrue(passed);
-    }
+        Specification spec = currentProject.findSpecification(specName);
 
-    public void printProcessOutput(boolean passed) {
-        if (!passed) {
-            System.out.println("*************** Process output start************");
-            System.out.println(currentProject.getLastProcessStdout());
-            System.out.println("*************** Process output end************");
-        }
+        assertThat(spec).isNotNull();
+        assertThat(currentProject.executeSpec(specName))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the spec <spec name> with scenario index <scenario index> and ensure success")
     public void executeScenarioWithIndex(String specName, int scenarioIndex) throws Exception {
         Specification spec = currentProject.findSpecification(specName);
-        if (spec == null) {
-            throw new RuntimeException("Specified spec is not present : " + specName);
-        }
-        boolean passed = currentProject.executeSpecWithScenarioIndex(specName, scenarioIndex);
-        printProcessOutput(passed);
-        assertTrue(passed);
+
+        assertThat(spec).isNotNull();
+        assertThat(currentProject.executeSpecWithScenarioIndex(specName, scenarioIndex))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the spec <spec name> with row range <row range> and ensure success")
     public void executeScenarioWithRowRange(String specName, String rowRange) throws Exception {
         Specification spec = currentProject.findSpecification(specName);
-        if (spec == null) {
-            throw new RuntimeException("Specified spec is not present : " + specName);
-        }
-        boolean passed = currentProject.executeSpecWithRowRange(specName, rowRange);
-        printProcessOutput(passed);
-        assertTrue(passed);
+
+        assertThat(spec).isNotNull();
+        assertThat(currentProject.executeSpecWithRowRange(specName, rowRange))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the spec <spec name> and ensure failure")
     public void executeSpecAndEnsureFailure(String specName) throws Exception {
-        assertTrue(!executeSpec(specName));
-    }
-
-    public boolean executeSpec(String specName) throws Exception {
         Specification spec = currentProject.findSpecification(specName);
-        if (spec == null) {
-            throw new RuntimeException("Specified spec is not present : " + specName);
-        }
-        boolean passed = currentProject.executeSpec(specName);
-        printProcessOutput(passed);
-        return passed;
+
+        assertThat(spec).isNotNull();
+        assertThat(currentProject.executeSpec(specName))
+                .isFalse()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Execute the tags <tags> in spec <spec name> and ensure success")
     public void executeTagsAndEnsureSuccess(String tags, String specName) throws IOException, InterruptedException {
         Specification spec = currentProject.findSpecification(specName);
-        if (spec == null) {
-            throw new RuntimeException("Specified spec is not present : " + specName);
-        }
-        boolean passed = currentProject.executeTagsInSpec(tags, specName);
-        printProcessOutput(passed);
-        assertTrue(passed);
+
+        assertThat(spec).isNotNull();
+        assertThat(currentProject.executeTagsInSpec(tags, specName))
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Check for validation errors in the project and ensure failure")
     public void validateAndEnsureFailure() throws IOException, InterruptedException {
-        boolean passed = currentProject.validate();
-        printProcessOutput(passed);
-        assertFalse(passed);
+        assertThat(currentProject.validate())
+                .isFalse()
+                .withFailMessage(getFormattedProcessOutput());
     }
 
     @Step("Check for validation errors in the project and ensure success")
     public void validateAndEnsureSuccess() throws IOException, InterruptedException {
-        boolean passed = currentProject.validate();
-        printProcessOutput(passed);
-        assertTrue(passed);
+        assertThat(currentProject.validate())
+                .isTrue()
+                .withFailMessage(getFormattedProcessOutput());
+    }
+
+    private String getFormattedProcessOutput()
+    {
+        return "*************** Process output start************\n" +
+                currentProject.getLastProcessStdout() +
+                "\n*************** Process output end************\n";
     }
 }
