213528bbf40d1a26c3f6c48adb79d96d98c76aac
sriv <srikanth.ddit@gmail.com>
2016-07-26 15:20:51 +0530

    custom screenshot spec now uses phantomjs to assert

---
+++ resources/get-screenshot.js
@@ -0,0 +1,32 @@
+"use strict";
+var page = require('webpage').create(),
+  system = require('system'),
+  url, stepText;
+
+if (system.args.length !== 3) {
+    console.log('Usage: html-report-screenshot.js <url> <step_text>');
+    phantom.exit(1);
+}
+
+url = system.args[1];
+stepText = system.args[2];
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+page.open(url, function(status) {
+    if (status === "success") {
+        page.evaluate(function(s) {
+            [].forEach.call(document.getElementsByClassName("step-txt"), function(element) {
+                if(element.innerText.indexOf(s)>=0) {
+                    console.log(element.nextElementSibling.getElementsByClassName('screenshot-thumbnail')[0].getAttribute('src'));
+                }
+            });
+        }, stepText);
+        phantom.exit(0);
+    } else {
+      console.log("failed");
+      phantom.exit(1);
+    }
+});
--- src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
@@ -3,21 +3,30 @@ package com.thoughtworks.gauge.test.implementation;
 import com.thoughtworks.gauge.Step;
 import com.thoughtworks.gauge.Table;
 import com.thoughtworks.gauge.test.common.Util;
-import org.assertj.core.api.Assertions;
 
 import java.io.IOException;
 import java.util.Base64;
 
 import static com.thoughtworks.gauge.test.common.GaugeProject.currentProject;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HtmlReport {
     @Step("Generated html report should have <some screenshot> for <table>")
     public void verifyCustomScreenshot(String stubScreenshot, Table stepTexts) throws IOException {
-        String resultPath = Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "js", "result.js");
+        for (String stepText : stepTexts.getColumnValues(0)) {
+            String path = this.getClass().getClassLoader().getResource("get-screenshot.js").getPath();
+            String expected = "data:image/png;base64," + Base64.getEncoder().encodeToString(stubScreenshot.getBytes());
+            String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
+            Process process = new ProcessBuilder("phantomjs", path, reportsPath, stepText).start();
 
-        String result = Util.read(resultPath);
-        result = result.substring(result.indexOf("var gaugeExecutionResult = ") + 1, result.indexOf(";"));
+            String screenshotText = convertStreamToString(process.getInputStream());
 
-        Assertions.assertThat(result).contains("\"screenShot\":\"" + Base64.getEncoder().encodeToString(stubScreenshot.getBytes())+"\"");
+            assertThat(screenshotText).contains(expected);
+        }
+    }
+
+    private static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
