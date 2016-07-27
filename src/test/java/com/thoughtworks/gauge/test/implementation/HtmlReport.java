3b76dde86a1e024991fd38b4d5b408f7ce84357b
sriv <srikanth.ddit@gmail.com>
2016-07-27 09:59:57 +0530

    use HTMLUnit for verifying screenshot
    get rid of phantomjs dependency
    delete obsolete get-screenshot.js files

--- src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.gauge.test.implementation;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.thoughtworks.gauge.Step;
 import com.thoughtworks.gauge.Table;
 import com.thoughtworks.gauge.test.common.Util;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Node;
+import se.fishtank.css.selectors.Selectors;
+import se.fishtank.css.selectors.dom.W3CNode;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
+import java.util.logging.Level;
 
 import static com.thoughtworks.gauge.test.common.GaugeProject.currentProject;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,20 +21,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class HtmlReport {
     @Step("Generated html report should have <some screenshot> for <table>")
     public void verifyCustomScreenshot(String stubScreenshot, Table stepTexts) throws IOException {
-        for (String stepText : stepTexts.getColumnValues(0)) {
-            String path = this.getClass().getClassLoader().getResource("get-screenshot.js").getPath();
-            String expected = "data:image/png;base64," + Base64.getEncoder().encodeToString(stubScreenshot.getBytes());
-            String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
-            Process process = new ProcessBuilder("phantomjs", path, reportsPath, stepText).start();
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
 
-            String screenshotText = convertStreamToString(process.getInputStream());
+        String expected = "data:image/png;base64," + Base64.getEncoder().encodeToString(stubScreenshot.getBytes());
+        String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
 
-            assertThat(screenshotText).contains(expected);
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage(reportsPath);
+            Selectors selectors = new Selectors(new W3CNode(page.getDocumentElement()));
+
+            List<Node> divs = selectors.querySelectorAll(".step-txt");
+            for (Node div : divs) {
+                for (String stepText : stepTexts.getColumnValues(0)) {
+                    if(div.getTextContent().contains(stepText)){
+                        Selectors errorSelectors = new Selectors(new W3CNode(div.getParentNode()));
+                        Node screenshotThumbnail = (Node) errorSelectors.querySelectorAll("img.screenshot-thumbnail").get(0);
+                        String actual = screenshotThumbnail.getAttributes().getNamedItem("src").getTextContent();
+                        assertThat(actual).isEqualTo(expected);
+                    }
+                }
+            }
         }
-    }
-
-    private static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
     }
 }
