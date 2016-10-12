776c38c0c9752598f2a29b0b75c129602936c27c
mahendrakariya <mahendrakariya@gmail.com>
2016-10-12 20:20:58 +0530

    Fix HTML report tests

--- src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/HtmlReport.java
@@ -11,58 +11,67 @@ import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Node;
 import se.fishtank.css.selectors.Selectors;
 import se.fishtank.css.selectors.dom.W3CNode;
+
 import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 import java.util.logging.Level;
+
 import static com.thoughtworks.gauge.test.common.GaugeProject.currentProject;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class HtmlReport {
 
+    private String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
+
     @Step("Generated html report should have <some screenshot> for <table>")
     public void verifyCustomScreenshot(String stubScreenshot, Table stepTexts) throws IOException {
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
         java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
         String expected = "data:image/png;base64," + Base64.getEncoder().encodeToString(stubScreenshot.getBytes());
-        String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
-        try (final WebClient webClient = new WebClient()) {
-            final HtmlPage page = webClient.getPage(reportsPath);
-            Selectors selectors = new Selectors(new W3CNode(page.getDocumentElement()));
-            List<Node> divs = selectors.querySelectorAll(".step-txt");
-            for (Node div : divs) {
-                for (String stepText : stepTexts.getColumnValues(0)) {
-                    if (div.getTextContent().contains(stepText)) {
-                        Selectors errorSelectors = new Selectors(new W3CNode(div.getParentNode()));
-                        Node screenshotThumbnail = (Node) errorSelectors.querySelectorAll("img.screenshot-thumbnail").get(0);
-                        String actual = screenshotThumbnail.getAttributes().getNamedItem("src").getTextContent();
-                        assertThat(actual).isEqualTo(expected);
-                    }
-                }
-            }
+
+        final WebClient webClient = new WebClient();
+        final HtmlPage page = webClient.getPage(reportsPath);
+        Selectors selectors = new Selectors(new W3CNode(page.getDocumentElement()));
+        List<Node> divs = selectors.querySelectorAll(".step-txt");
+
+        for (Node div : divs) {
+            stepTexts.getColumnValues(0).stream().filter(stepText -> div.getTextContent().contains(stepText)).forEach(stepText -> {
+                Selectors errorSelectors = new Selectors(new W3CNode(div.getParentNode()));
+                Node screenshotThumbnail = (Node) errorSelectors.querySelectorAll("img.screenshot-thumbnail").get(0);
+                String actual = screenshotThumbnail.getAttributes().getNamedItem("src").getTextContent();
+                assertThat(actual).isEqualTo(expected);
+            });
         }
     }
 
     @Step("verify statistics in html with <statistics>")
     public void verifyStatistics(Table statistics) throws IOException {
-        String reportsPath = "file://" + Util.combinePath(currentProject.getProjectDir().getAbsolutePath(), "reports", "html-report", "index.html");
-        try (final WebClient webClient = new WebClient()) {
-            final HtmlPage page = webClient.getPage(reportsPath);
-            Selectors selectors = new Selectors(new W3CNode(page.getDocumentElement()));
-            String expectedTotalCount = ((HtmlDivision) selectors.querySelectorAll(".total-specs").get(0)).getFirstChild().asText();
-            String expectedPassCount = ((HtmlListItem) selectors.querySelectorAll(".pass").get(0)).getFirstChild().asText();
-            String expectedFailCount = ((HtmlListItem) selectors.querySelectorAll(".fail").get(0)).getFirstChild().asText();
-            String expectedSkippedCount = ((HtmlListItem) selectors.querySelectorAll(".skip").get(0)).getFirstChild().asText();
-            String actualTotalCount = statistics.getTableRows().get(0).getCell("totalCount");
-            String actualPassCount = statistics.getTableRows().get(0).getCell("passCount");
-            String actualFailCount = statistics.getTableRows().get(0).getCell("failCount");
-            String actualSkippedCount = statistics.getTableRows().get(0).getCell("skippedCount");
-            assertEquals("Total count:",expectedTotalCount, actualTotalCount);
-            assertEquals("Pass count:",expectedPassCount, actualPassCount);
-            assertEquals("Fail count:",expectedFailCount, actualFailCount);
-            assertEquals("Skipped count:",expectedSkippedCount, actualSkippedCount);
-        }
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
+        final WebClient webClient = new WebClient();
+        final HtmlPage page = webClient.getPage(reportsPath);
+        Selectors selectors = new Selectors(new W3CNode(page.getDocumentElement()));
+
+        String expectedTotalCount = ((HtmlDivision) selectors.querySelectorAll(".total-specs").get(0)).getFirstChild().asText();
+        String actualTotalCount = statistics.getTableRows().get(0).getCell("totalCount");
+        assertEquals("Total count:", expectedTotalCount, actualTotalCount);
+
+        String expectedPassCount = ((HtmlListItem) selectors.querySelectorAll(".pass").get(0)).getFirstChild().asText();
+        String actualPassCount = statistics.getTableRows().get(0).getCell("passCount");
+        assertEquals("Pass count:", expectedPassCount, actualPassCount);
+
+        String expectedFailCount = ((HtmlListItem) selectors.querySelectorAll(".fail").get(0)).getFirstChild().asText();
+        String actualFailCount = statistics.getTableRows().get(0).getCell("failCount");
+        assertEquals("Fail count:", expectedFailCount, actualFailCount);
+
+        String expectedSkippedCount = ((HtmlListItem) selectors.querySelectorAll(".skip").get(0)).getFirstChild().asText();
+        String actualSkippedCount = statistics.getTableRows().get(0).getCell("skippedCount");
+        assertEquals("Skipped count:", expectedSkippedCount, actualSkippedCount);
     }
 }
