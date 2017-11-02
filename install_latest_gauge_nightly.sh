f10652c565344da9ce75fd281e8298c32a414fed
BugDiver <vinaysh@thoughtworks.com>
2017-11-02 17:26:12 +0530

    Updating gauge nightly installation script

--- install_latest_gauge_nightly.sh
+++ install_latest_gauge_nightly.sh
@@ -9,10 +9,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
 	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_NIGHTLY_VERSION-linux.$BIT.zip"
 	GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/Gauge/download_file?file_path=linux%2F$GAUGE_FILE_NAME"
 	wget $GAUGE_DOWNLOAD_URL -O $GAUGE_FILE_NAME
-	OUTPUT_DIR="./gauge_$GAUGE_LATEST_NIGHTLY_VERSION"
-	unzip $GAUGE_FILE_NAME -d $OUTPUT_DIR
-	cd $OUTPUT_DIR
-	/bin/bash install.sh $1
+	unzip $GAUGE_FILE_NAME -d $GAUGE_PREFIX
 elif [[ "$unamestr" == 'Darwin' ]]; then
 	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_NIGHTLY_VERSION-darwin.$BIT.pkg"
 	GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/Gauge/download_file?file_path=darwin%2F$GAUGE_FILE_NAME"
