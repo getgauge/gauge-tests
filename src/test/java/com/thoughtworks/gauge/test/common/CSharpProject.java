1b4436b580875d01483f152c9d1c8c9761607df2
sriv <srikanth.ddit@gmail.com>
2018-05-11 07:35:58 +0530

    fix csharp custom screengrabber implementation

--- src/test/java/com/thoughtworks/gauge/test/common/CSharpProject.java
+++ src/test/java/com/thoughtworks/gauge/test/common/CSharpProject.java
@@ -167,7 +167,7 @@ public class CSharpProject extends GaugeProject {
         sb.append("\n");
         sb.append("public class " + className + " : ICustomScreenshotGrabber {\n");
         sb.append("\n");
-        sb.append("    byte[] ICustomScreenshotGrabber.TakeScreenShot() {\n");
+        sb.append("    public byte[] TakeScreenShot() {\n");
         sb.append("        return System.Text.Encoding.UTF8.GetBytes(\"" + stubScreenshot + "\");\n");
         sb.append("    }\n");
         sb.append("}");
