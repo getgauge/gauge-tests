5faa0b32b4655b44f4cb3112214c0db285da6b48
BugDiver <vinaysh@thoughtworks.com>
2019-10-10 11:50:57 +0530

    logging to out stream instead err.

--- src/test/java/com/thoughtworks/gauge/test/common/Hooks.java
+++ src/test/java/com/thoughtworks/gauge/test/common/Hooks.java
@@ -1,24 +1,21 @@
 package com.thoughtworks.gauge.test.common;
 
-import com.thoughtworks.gauge.AfterScenario;
-import com.thoughtworks.gauge.BeforeSuite;
-import com.thoughtworks.gauge.ExecutionContext;
-import org.apache.commons.io.FileUtils;
+import static com.thoughtworks.gauge.test.common.GaugeProject.getCurrentProject;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import static com.thoughtworks.gauge.test.common.GaugeProject.getCurrentProject;
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.ExecutionContext;
+
+import org.apache.commons.io.FileUtils;
 
 public class Hooks {
     @AfterScenario
     public void tearDown(ExecutionContext context) throws IOException {
         if(getCurrentProject()==null)
         {
-            System.err.println("Current project is unavailable");
+            System.out.println("Current project is unavailable");
             return;
         }
         File dir = getCurrentProject().getProjectDir();
@@ -40,13 +37,13 @@ public class Hooks {
             try {
                 getCurrentProject().getService().getGaugeProcess().destroyForcibly().waitFor();
             } catch (InterruptedException e) {
-                System.err.println(String.format("Unable to stop gauge process for %s; reason : %s", dir.getAbsolutePath(), e.getMessage()));
+                System.out.println(String.format("Unable to stop gauge process for %s; reason : %s", dir.getAbsolutePath(), e.getMessage()));
             }
         }
         try {
             FileUtils.deleteDirectory(dir);
         } catch (IOException e) {
-            System.err.println(String.format("Could not delete project directory %s; reason : %s", dir.getAbsolutePath(), e.getMessage()));
+            System.out.println(String.format("Could not delete project directory %s; reason : %s", dir.getAbsolutePath(), e.getMessage()));
         }
     }
 }
