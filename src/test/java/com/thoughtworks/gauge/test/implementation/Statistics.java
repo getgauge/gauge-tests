92cb13fa702a36d0b69e5b11c27028e8214f2aec
Chad Wilson <chadw@thoughtworks.com>
2025-02-04 00:23:19 +0800

    Fix typo
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>

--- src/test/java/com/thoughtworks/gauge/test/implementation/Statistics.java
+++ src/test/java/com/thoughtworks/gauge/test/implementation/Statistics.java
@@ -4,23 +4,21 @@ import com.thoughtworks.gauge.Step;
 import com.thoughtworks.gauge.Table;
 import com.thoughtworks.gauge.TableRow;
 
-import java.io.IOException;
-
 import static com.thoughtworks.gauge.test.common.GaugeProject.getCurrentProject;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Statistics {
 
     @Step("Statistics generated should have <table>")
-    public void statisticsGenereatedShouldHave(Table expectedStatisticsData) throws IOException {
+    public void statisticsGeneratedShouldHave(Table expectedStatisticsData) {
         String output = getCurrentProject().getStdOut();
         for (TableRow row : expectedStatisticsData.getTableRows()) {
             StringBuilder statisticsData = new StringBuilder();
-            statisticsData.append(row.getCell("Statistics name") + ": ");
-            statisticsData.append(row.getCell("executed") + " executed ");
-            statisticsData.append(row.getCell("passed") + " passed ");
-            statisticsData.append(row.getCell("failed") + " failed ");
-            statisticsData.append(row.getCell("skipped") + " skipped");
+            statisticsData.append(row.getCell("Statistics name")).append(": ");
+            statisticsData.append(row.getCell("executed")).append(" executed ");
+            statisticsData.append(row.getCell("passed")).append(" passed ");
+            statisticsData.append(row.getCell("failed")).append(" failed ");
+            statisticsData.append(row.getCell("skipped")).append(" skipped");
             assertThat(output.replaceAll("\t", " ")).contains(statisticsData);
         }
     }
